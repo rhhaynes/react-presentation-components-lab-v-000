2b2516581ee48c3674dd3b0f2af449856c1e386c
R. Harris Haynes <harrishaynes@gmail.com>
2018-07-12 16:07:49 +0000

    automatically backed up by learn

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -3,6 +3,6 @@ import React from 'react';
 export default class SimpleComponent extends React.Component {
   constructor() {
     super();
-    this.state = { mood: '' };
+    this.state = { mood: 'happy' };
   }
 }
